521e9f5fa0ee16add58b20ef46bf2075413d62ff
marcosab10 <marcosab10@yahoo.com.br>
2018-02-07 23:34:57 -0200

    Tela Negociacao

---
+++ BitCoinMentor/NegociarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NegociarViewController.swift
+//  BitCoinMentor
+//
+//  Created by Marcos Faria on 07/02/18.
+//  Copyright © 2018 Marcos Faria. All rights reserved.
+//
+
+import UIKit
+
+class NegociarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
